40fbf8da8cfd0ae2b7b644527b3518ffb63c999b
Ben Neely <ben@bloc.io>
2015-05-21 22:31:37 -0600

    Adding a new Toolbar checkpoint

---
+++ BlocBrowser/BlocBrowser/AwesomeFloatingToolbar.h
@@ -0,0 +1,29 @@
+//
+//  AwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Ben Neely on 5/21/15.
+//  Copyright (c) 2015 Ben Neely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AwesomeFloatingToolbar;
+
+@protocol AwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+- (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
+
+@end
+
+@interface AwesomeFloatingToolbar : UIView
+
+- (instancetype) initWithFourTitles:(NSArray *)titles;
+
+- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
+
+@property (nonatomic, weak) id <AwesomeFloatingToolbarDelegate> delegate;
+
+@end
